d097cc9be2790ce9b5483e9c6cb5980d6914e27a
xmen2100 <119138813+xmen2100@users.noreply.github.com>
2026-05-04 21:09:25 -0700

    Move api.js to services folder

---
+++ services/api.js
@@ -0,0 +1,29 @@
+import axios from 'axios';
+
+const BASE_URL = 'https://api.tvmaze.com';
+
+export async function searchShows(keyword) {
+    try {
+
+        const response = await axios.get(`${BASE_URL}/search/shows?q=${keyword}`);
+        
+        return response.data;
+        
+    } catch (error) {
+        console.error('There was an error getting the search results:', error.message);
+        return [];
+    } 
+}
+
+export async function getShowById(id) {
+    try {
+
+        const response = await axios.get(`${BASE_URL}/shows/${id}`);
+
+        return response.data;
+
+    } catch (error) {
+        console.error('There was an error getting the show details:', error.message);
+        return null;
+    }
+}
