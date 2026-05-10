572899113a7a28fd73c03f9eceba609143a6405a
JessieGuijos <jguijos3@calstatela.edu>
2026-05-10 16:21:34 -0700

    implemented GET /<topic>/:id

--- routes/shows.js
+++ routes/shows.js
@@ -30,12 +30,6 @@ router.get('/', async (req, res) => {
         // search tvmaze api by keyword
         const results = await searchShows(keyword);
 
-        // format results for minimal clean JSON response
-        const shows = _formatShows(results);
-
-        // return minimal results
-        res.json(shows);
-
         // save keyword into mongo only if unique
         const cursor = await db.find('SearchHistoryKeyword', { keyword });
         const existingKeyword = await cursor.next();
@@ -43,11 +37,43 @@ router.get('/', async (req, res) => {
         if (!existingKeyword) {
             await db.insert('SearchHistoryKeyword', { keyword });
         }
+        // format results for minimal clean JSON response
+        const shows = _formatShows(results);
+
+        // return minimal results
+        res.json(shows);
     } catch (error) {
         // handle unexpected errors
         res.status(500).json({ error });
     }
 });
 
-// ----- implement GET /<topic>/:id here -----
-// ----- ex: localhost:8888/shows/590 -----
+// ex: localhost:3000/shows/590
+router.get('/:id', async (req, res) => {
+    try {
+        const { id } = req.params;
+
+        if (!id) {
+            return res
+                .status(400)
+                .json({ error: 'show id path parameter is required' });
+        }
+
+        const show = await getShowById(id);
+
+        if (!show) {
+            return res
+                .status(404)
+                .json({ error: 'show not found' });
+        }
+
+        res.json(show);
+
+    } catch (error) {
+        res.status(500).json({
+            error: 'There was an error getting show details'
+        });
+    }
+});
+
+export default router;
